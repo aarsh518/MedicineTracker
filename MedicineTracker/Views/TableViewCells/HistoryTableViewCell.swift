e3ae22851c06811c93f0e5d5289d507c301d9496
Aarsh Parekh <aarsh.parekh@live.com>
2020-08-07 00:11:38 +0530

    - Initial project structure setup

---
+++ MedicineTracker/Views/TableViewCells/HistoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HistoryTableViewCell.swift
+//  MedicineTracker
+//
+//  Created by Aarsh Parekh on 07/08/20.
+//  Copyright © 2020 Aarsh. All rights reserved.
+//
+
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
